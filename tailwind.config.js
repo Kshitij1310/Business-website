aa027b33cb393c9a57034be1c96b80a986864e9f
SHITIJ YADAV <149371568+Kshitij1310@users.noreply.github.com>
2026-03-02 19:52:43 +0530

    Update README.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -57,5 +61,5 @@ export default {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
 
